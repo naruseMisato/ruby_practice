6a9fb392599d506570ba36d92cc25521657f4a14
naruseMisato <misato.naruse@jmty.jp>
2020-02-12 03:49:00 +0900

    add

--- チェリー本_第7章.rb
+++ チェリー本_第7章.rb
@@ -8,9 +8,35 @@
 # init:「initialize」でinitが呼ばれる
 
 class Mubappe
-  def initialize
-    p "aaaaa"
+  def initialize(age)
+    p "ムバッペは#{age}歳です"
+  end
+
+  def get_name
+    "むーちゃん"
   end
 end
 
- puts Mubappe.new　# -> "aaaaa"
+pet = Mubappe.new(1.5)
+p pet # -> "ムバッペは1.5歳です"
+p pet.get_name # -> "むーちゃん"
+
+# インスタンス変数
+
+class Mubappe2
+  def initialize(age, name)
+    # @name = name
+    p "#{name}は#{age}歳です"
+  end
+
+  def get_name
+    @name
+  end
+end
+
+pet2 = Mubappe2.new(1.5, "むーちゃん")
+p pet2 # -> "むーちゃんは1.5歳です"
+p pet2.get_name # -> "むーちゃん"
+
+# ※↑init内の「@name = name」を消すと、
+p pet2.get_name # -> nil
