46cbed8f0926a4f2e0cb9bc90c49f323b3b83de4
naruseMisato <misato.naruse@jmty.jp>
2020-02-12 03:29:50 +0900

    add

---
+++ チェリー本_第7章.rb
@@ -0,0 +1,16 @@
+# チェリー本の覚えたい要点を書いてくよ＼＼\\٩( 'ω' )و //／／
+# 特有の感じとか、swiftと違うところとかを意識する
+
+# この単元はクラスとかについて
+
+# ==============================================
+
+# init:「initialize」でinitが呼ばれる
+
+class Mubappe
+  def initialize
+    p "aaaaa"
+  end
+end
+
+ puts Mubappe.new　# -> "aaaaa"
